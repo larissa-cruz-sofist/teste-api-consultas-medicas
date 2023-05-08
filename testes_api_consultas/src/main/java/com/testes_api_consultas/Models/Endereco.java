eaa8ec69e70909c628c20d5e03a6de270deab592
Larissa Cruz <larissa.cruz@sofist.co>
2023-05-08 10:46:49 -0300

    primeras alteracoes com faker

--- testes_api_consultas/src/main/java/com/testes_api_consultas/Models/Endereco.java
+++ testes_api_consultas/src/main/java/com/testes_api_consultas/Models/Endereco.java
@@ -1,6 +1,13 @@
 package com.testes_api_consultas.Models;
 
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+
 public class Endereco {
+
 
     public String logradouro;
     public String bairro;
@@ -10,16 +17,31 @@ public class Endereco {
     public String numero;
     public String complemento;
 
+    
     public Endereco() {
-        
-        this.logradouro = "rua indaia";
-        this.bairro = "vila mariana";
-        this.cep = "56987142";
-        this.cidade = "sao paulo";
-        this.uf = "sp";
-        this.numero = "150";
-        this.complemento = "bloco 2, apto 13";
+
+
     }
 
-    
-}
+
+    public Endereco criarEndereco() {
+
+        Faker faker = new Faker(new Locale("pt-BR"));
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("pt-BR"), new RandomService());
+        
+        this.logradouro = faker.address().streetName();
+        this.bairro = faker.address().lastName();
+        this.cep = fakeValuesService.numerify("########");
+        //this.cep = faker.address().zipCode();
+        this.cidade = faker.address().city();
+        this.uf = faker.address().stateAbbr();
+        this.numero = faker.address().buildingNumber();
+        this.complemento = faker.address().secondaryAddress();
+
+        return this;
+
+    }
+
+
+} 
+
--- testes_api_consultas/src/main/java/com/testes_api_consultas/Models/Medico.java
+++ testes_api_consultas/src/main/java/com/testes_api_consultas/Models/Medico.java
@@ -1,5 +1,11 @@
 package com.testes_api_consultas.Models;
 
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+
 public class Medico {
     
     public String id;
@@ -20,12 +26,15 @@ public class Medico {
 
     public Medico criarMedico() {
 
-        this.nome = "Pamela Silva";
-        this.email = "pamelasilva@vold.med";
-        this.crm = "555588";
-        this.telefone = "965408551";
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("pt-BR"), new RandomService());
+        Faker faker = new Faker(new Locale("pt-BR"));
+
+        this.nome = faker.name().fullName();
+        this.email = fakeValuesService.bothify("???????##@vold.med");
+        this.crm =  fakeValuesService.numerify("######");
+        this.telefone = faker.phoneNumber().cellPhone();
         this.especialidade = "DERMATOLOGIA";
-        this.endereco = new Endereco();
+        this.endereco = new Endereco().criarEndereco();
 
         return this;
     }
