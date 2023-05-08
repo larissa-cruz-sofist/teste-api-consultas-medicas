2934087805e7260f0e6aaadeb7ed533f40b69216
Larissa Cruz <larissa.cruz@sofist.co>
2023-05-08 15:04:27 -0300

    criacao enum EspecialidadeMedico

---
+++ testes_api_consultas/src/main/java/com/testes_api_consultas/Models/EspecialidadeMedico.java
@@ -0,0 +1,10 @@
+package com.testes_api_consultas.Models;
+
+public enum EspecialidadeMedico {
+
+    ORTOPEDIA,
+	CARDIOLOGIA,
+	GINECOLOGIA,
+	DERMATOLOGIA;
+    
+}
--- testes_api_consultas/src/main/java/com/testes_api_consultas/Models/Medico.java
+++ testes_api_consultas/src/main/java/com/testes_api_consultas/Models/Medico.java
@@ -33,7 +33,7 @@ public class Medico {
         this.email = fakeValuesService.bothify("???????##@vold.med");
         this.crm =  fakeValuesService.numerify("######");
         this.telefone = faker.phoneNumber().cellPhone();
-        this.especialidade = "DERMATOLOGIA";
+        this.especialidade = EspecialidadeMedico.CARDIOLOGIA.toString();
         this.endereco = new Endereco().criarEndereco();
 
         return this;
